dde3fa284aa08ebc6ac8398390ce8b4667a9b1ec
litencatt <ncl0709@gmail.com>
2017-01-12 00:06:14 +0900

    Rails5対応したRSpecの導入のためのGemfile変更

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '5.0.0.1'
+gem 'rake'
 gem 'puma',         '3.4.0'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.0.0'
@@ -12,7 +13,8 @@ gem 'jbuilder',     '2.4.1'
 group :development, :test do
   gem 'sqlite3', '1.3.11'
   gem 'byebug',  '9.0.0', platform: :mri
-  gem 'rspec-rails', '~> 2.14.0'
+  gem 'rspec-rails'
+  gem 'rspec-core'
   gem 'factory_girl_rails', '~> 4.2.1'
 end
 
@@ -24,11 +26,11 @@ group :development do
 end
 
 group :test do
-  gem 'faker', '~> 1.1.2'
-  gem 'capybara', '~> 2.1.0'
-  gem 'database_cleaner', '~> 1.0.1'
-  gem 'launchy', '~> 2.3.0'
-  gem 'selenium-webdriver', '~>2.45.0'
+  gem 'faker'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'selenium-webdriver'
 end
 
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+ActiveSupport::Deprecation.silenced = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,16 +12,19 @@ module Railstutorial5
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+=begin
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+=end
   end
 end
 
-config.generators do |g|
-  g.test_framework :rspec,
-    fixtures: true,
-    view_specs: false,
-    helper_specs: false,
-    routing_specs: false,
-    controller_specs: true,
-    request_specs: false
-  g.fixture_replacement :factory_girl, dir: "spec/factories"
-end
+
