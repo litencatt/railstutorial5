8dd54ad40814e0d10e120704bd8b244eac1650c9
Nakamura Kosuke <nakamura.k@pepabo.com>
2017-01-12 20:38:48 +0900

    finished until 3.4.3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   root 'application#goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
