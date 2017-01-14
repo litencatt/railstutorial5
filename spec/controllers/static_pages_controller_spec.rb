66889352aecd2a23aab608f1c8c7edbdb13a0632
litencatt <ncl0709@gmail.com>
2017-01-14 22:46:43 +0900

    Finished chapter3

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 describe StaticPagesController do
   describe 'GET home' do
-    it 'responce is 200 OK'
+    it 'responce is 200 OK' do
+      get :home
+      expect(response.status).to eq 200
+    end
+  end
+  describe 'GET help' do
+    it 'responce is 200 OK' do
+      get :help
+      expect(response.status).to eq 200
+    end
+  end
+  describe 'GET about' do
+    it 'responce is 200 OK' do
+      get :about
+      expect(response.status).to eq 200
+    end
+  end
+  describe 'GET contact' do
+    it 'responce is 200 OK' do
+      get :contact
+      expect(response.status).to eq 200
+    end
   end
 end
