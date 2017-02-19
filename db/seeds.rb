70a8f2fb83d676a6decc8195b6082bf8cecb23e3
litencatt <ncl0709@gmail.com>
2017-02-19 23:25:08 +0900

    Finished 11.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "password", password_confirmation: "password", admin: true)
+User.create!(
+  name: "Example User",
+  email: "example@railstutorial.org",
+  password: "password",
+  password_confirmation: "password",
+  admin: true,
+  activated: true,
+  activate_at: Time.zone.now
+)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name, email: email, password: "password", password_confirmation: "password")
+  User.create!(name: name,
+               email: email,
+               password: "password",
+               password_confirmation: "password",
+               activated: true,
+               activate_at: Time.zone.now
+              )
 end
