a1661249d6f73ffb86b2a6904e81c6ef673bd1f2
Nakamura Kosuke <ncl0709@gmail.com>
2017-03-12 14:01:46 +0900

    Finished chapter 12

---
+++ spec/integration/password_reset_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe PasswordResetsTest do
+  describe 'password resets' do
+
+  end
+end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe UserMailer, type: :mailer do
+describe UserMailer, type: :mailer do
   describe "account_activation" do
     it do
       user = create(:user)
@@ -11,6 +11,18 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.from).to eq ["noreply@example.com"]
       expect(mail.body.encoded).to match user.name
       expect(mail.body.encoded).to match user.activation_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
+    end
+  end
+  describe "password_reset" do
+    it do
+      user = create(:user)
+      user.reset_token = User.new_token
+      mail = UserMailer.password_reset(user)
+      expect(mail.subject).to eq "Password reset"
+      expect(mail.to).to eq [user.email]
+      expect(mail.from).to eq ["noreply@example.com"]
+      expect(mail.body.encoded).to match user.reset_token
       expect(mail.body.encoded).to match CGI.escape(user.email)
     end
   end
