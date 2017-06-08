17158daa46eb107865bd63f6428888f350e2bbf3
Nakamura Kosuke <ncl0709@gmail.com>
2017-06-08 23:04:58 +0900

    commit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,10 @@
 class RegistrationsController < ApplicationController
   def new
-    @domains = %w(a.jp b.co.jp c.or.jp d.asia)
+    @domains = %w(a.jp b.jp c.or.jp d.asia)
   end
 
   def create
+    binding.pry
   end
 #  def next
 #    need_registrations.each do |domain|
@@ -15,4 +16,9 @@ class RegistrationsController < ApplicationController
 #
 #  def prev
 #  end
+  private
+
+  def create_params
+    params.require(:gjp, :ojp, :cojp)
+  end
 end
