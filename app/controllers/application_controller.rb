df9db53cb18232f684f556220161e026fa9aeb33
Nakamura Kosuke <ncl0709@gmail.com>
2017-10-09 15:22:01 +0900

    fix coding style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 
 end
