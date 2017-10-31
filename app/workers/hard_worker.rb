4e3df4cccd3bbd6169dd943a48926d66d20090e3
Nakamura Kosuke <ncl0709@gmail.com>
2017-10-31 23:50:01 +0900

    add worker process

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,7 +1,17 @@
 class HardWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(complexity)
+    case complexity
+    when "super_hard"
+      sleep  20
+      puts "Realy took quire a bit of effort"
+    when "hard"
+      sleep  10
+      puts "That was a bit of work"
+    else
+      sleep 1
+      puts "That wasn't a lot of effort"
+    end
   end
 end
